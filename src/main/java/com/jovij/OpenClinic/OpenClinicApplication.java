0da7678eb244f2a6858752ccf82f04ef59d745e2
João Victor (Jovi) <nean@protonmail.com>
2025-11-04 20:32:47 -0300

    Initial commit

---
+++ src/main/java/com/jovij/OpenClinic/OpenClinicApplication.java
@@ -0,0 +1,13 @@
+package com.jovij.OpenClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jovij/OpenClinic/OpenClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovij.OpenClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
