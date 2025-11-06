a36064b58f3ba84cce9f846fa519f55599d8107b
João Victor (Jovi) <nean@protonmail.com>
2025-11-06 16:05:16 -0300

    chore: removing unused imports

--- src/main/java/com/jovij/OpenClinic/Model/TicketAttendanceRecord.java
+++ src/main/java/com/jovij/OpenClinic/Model/TicketAttendanceRecord.java
@@ -3,7 +3,6 @@ package com.jovij.OpenClinic.Model;
 import com.jovij.OpenClinic.Model.Enums.TicketStatus;
 import com.jovij.OpenClinic.Model.Generics.GenericModel;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
