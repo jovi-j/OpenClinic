00689fa00bea58aece226f93d6b7a1f74cd1599a
Jovi <j.v.s.o1020@gmail.com>
2025-11-13 18:22:30 -0300

    fix: applying change in the DTOs folder layout to the ScheduleDTO import

--- src/main/java/com/jovij/OpenClinic/Controller/ScheduleController.java
+++ src/main/java/com/jovij/OpenClinic/Controller/ScheduleController.java
@@ -1,11 +1,14 @@
 package com.jovij.OpenClinic.Controller;
 
-import com.jovij.OpenClinic.Model.DTO.ScheduleDTO;
+import com.jovij.OpenClinic.Model.DTO.Schedule.ScheduleDTO;
 import com.jovij.OpenClinic.Model.Schedule;
 import com.jovij.OpenClinic.Service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/schedules")
