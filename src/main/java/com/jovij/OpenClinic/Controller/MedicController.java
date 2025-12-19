12fe95dab8474e7b64523aab47286f307f9ca91a
Jovi <j.v.s.o1020@gmail.com>
2025-12-19 15:57:28 -0300

    feat: update and delete of a medic

--- src/main/java/com/jovij/OpenClinic/Controller/MedicController.java
+++ src/main/java/com/jovij/OpenClinic/Controller/MedicController.java
@@ -1,16 +1,22 @@
 package com.jovij.OpenClinic.Controller;
 
 import com.jovij.OpenClinic.Model.DTO.Medic.MedicDTO;
-import com.jovij.OpenClinic.Model.Medic;
+import com.jovij.OpenClinic.Model.DTO.Medic.MedicResponseDTO;
+import com.jovij.OpenClinic.Model.DTO.Medic.MedicUpdateDTO;
 import com.jovij.OpenClinic.Service.MedicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/medics")
+@Tag(name = "Medics", description = "Endpoints for managing medics")
 public class MedicController {
 
     private final MedicService medicService;
@@ -21,15 +27,31 @@ public class MedicController {
     }
 
     @PostMapping
-    public ResponseEntity<Medic> create(@RequestBody MedicDTO medicDTO) {
-        Medic createdMedic = medicService.create(medicDTO);
-        URI location = URI.create("/medics/" + createdMedic.getId());
+    @Operation(summary = "Create a new medic")
+    public ResponseEntity<MedicResponseDTO> create(@RequestBody MedicDTO medicDTO) {
+        MedicResponseDTO createdMedic = medicService.create(medicDTO);
+        URI location = URI.create("/medics/" + createdMedic.id());
         return ResponseEntity.created(location).body(createdMedic);
     }
 
     @GetMapping
-    public ResponseEntity<Iterable<Medic>> listAll() {
-        Iterable<Medic> medics = medicService.listAll();
+    @Operation(summary = "List all medics")
+    public ResponseEntity<List<MedicResponseDTO>> listAll() {
+        List<MedicResponseDTO> medics = medicService.listAll();
         return ResponseEntity.ok(medics);
+    }
+
+    @PatchMapping("/{id}")
+    @Operation(summary = "Update a medic")
+    public ResponseEntity<MedicResponseDTO> update(@PathVariable UUID id, @RequestBody MedicUpdateDTO medicUpdateDTO) {
+        MedicResponseDTO updatedMedic = medicService.update(id, medicUpdateDTO);
+        return ResponseEntity.ok(updatedMedic);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a medic")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        medicService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
