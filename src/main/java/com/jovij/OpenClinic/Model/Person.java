504729cc9ab01f90171b7795ff95de9bf5d863f8
Jovi <j.v.s.o1020@gmail.com>
2025-12-19 15:54:05 -0300

    fix: implementing logical delete for the Person model in case of a deletion by repository

--- src/main/java/com/jovij/OpenClinic/Model/Person.java
+++ src/main/java/com/jovij/OpenClinic/Model/Person.java
@@ -5,12 +5,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDate;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE person SET deleted_at = now() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
 public class Person extends GenericModel {
     @Column(nullable = false)
     private String name;
