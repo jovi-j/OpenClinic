2226964020132a3cbf62d42720c28c27db9eae30
Jovi <j.v.s.o1020@gmail.com>
2025-12-19 15:57:11 -0300

    feat: OpenApi definition annotation for swagger

--- src/main/java/com/jovij/OpenClinic/OpenClinicApplication.java
+++ src/main/java/com/jovij/OpenClinic/OpenClinicApplication.java
@@ -1,9 +1,12 @@
 package com.jovij.OpenClinic;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "OpenClinic API", version = "1.0", description = "API for OpenClinic"))
 public class OpenClinicApplication {
 
 	public static void main(String[] args) {
