0dbe3da0186ebf4996bad6a87f9bbb70c2865252
Jovi <j.v.s.o1020@gmail.com>
2025-12-19 15:52:50 -0300

    chore!: removed Spring REST from the project

--- src/main/java/com/jovij/OpenClinic/Repository/MedicRepository.java
+++ src/main/java/com/jovij/OpenClinic/Repository/MedicRepository.java
@@ -1,15 +1,15 @@
 package com.jovij.OpenClinic.Repository;
 
 import com.jovij.OpenClinic.Model.Medic;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface MedicRepository extends CrudRepository<Medic, UUID> {
+public interface MedicRepository extends JpaRepository<Medic, UUID> {
     @Query("SELECT m FROM Medic m WHERE m.person.name = :doctorName")
     Optional<Medic> findByName(String doctorName);
 
